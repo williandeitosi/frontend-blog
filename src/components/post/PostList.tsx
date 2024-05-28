6241588e1bce14e4eb5e71697ca583e90a7c4bdf
willian <williandeitosi@gmail.com>
2024-05-28 20:18:43 -0300

    feat: post list

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -4,11 +4,13 @@ export function PostList() {
   const posts = [
     {
       title: 'agua',
-      description: ';kalhjfasdkljhdfalkj',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore qui sed doloribus a est quaerat voluptate tempore similique, officiis, cupiditate perspiciatis. Mollitia nisi perspiciatis dolor optio repellat ratione a ab!',
     },
     {
       title: 'pedra',
-      description: ';kalhjfasdkljhdfalkj',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore qui sed doloribus a est quaerat voluptate tempore similique, officiis, cupiditate perspiciatis. Mollitia nisi perspiciatis dolor optio repellat ratione a ab!',
     },
   ];
 
