cd798222af719a047c5c76ecff2efee4426c2aca
willian <williandeitosi@gmail.com>
2024-06-06 21:32:19 -0300

    fix:docker

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Footer } from './components/footer/Footer';
 import { Profile } from './components/profile/Profile';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
