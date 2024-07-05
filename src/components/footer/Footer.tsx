bc3aeacdd1595aa90efa011bb643fc47dc9d69fb
willian <williandeitosi@gmail.com>
2024-07-04 21:24:53 -0300

    remove(footer): footer css

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,13 @@
 import { useLocation } from 'react-router-dom';
-import './Footer.module.css';
+// import './Footer.module.css';
 
 export function Footer() {
   const location = useLocation();
   return (
-    location.pathname !== '/hidden' && <footer>&copy; 2024 Willian</footer>
+    location.pathname !== '/hidden' && (
+      <footer className='mt-8 h-20 text-black text-xs font-bold flex w-full justify-center items-center bg-gray-200 flex-shrink-0'>
+        &copy; 2024 Willian
+      </footer>
+    )
   );
 }
